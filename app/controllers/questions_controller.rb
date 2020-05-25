4e35e76a00cf06ff6a1eb23fd4e342febd0a0e62
Mariana Caramori <musardo@mail.com>
2020-05-25 12:51:56 -0300

    functional, front end done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,19 @@
 class QuestionsController < ApplicationController
   def ask
     @user_question = params[:user_question]
+    answer
   end
 
   def answer
+    @answer = ""
+    if @user_question
+      if @user_question.end_with?("?")
+        @answer = "Silly question, get dressed and go to work!"
+      elsif @user_question == "I am going to work"
+        @answer = "Great!"
+      else
+        @answer = "I don't care, get dressed and go to work!"
+      end
+    end
   end
 end
